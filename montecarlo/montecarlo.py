ba1d3f07ea0f6083a78023ceb3a2644f8128e214
Nick Mayhall <nmayhall@vt.edu>
2021-05-14 15:19:33 -0400

    added some junk files

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -6,6 +6,28 @@ Handles the primary functions
 """
 
 
+def canvas2(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format)
+
+    Replace this function and doc string for your own project
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution
+    """
+
+    quote = "The code is but a canvas to our imagination."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
@@ -28,7 +50,25 @@ def canvas(with_attribution=True):
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
 
+def testa():
+    """
+    This is just a test to see if I can do LaTeX
+
+    .. math::
+        \sum_{i=1}^{\\infty} x_{i}
+
+
+    Parammeters
+    -----------
+
+    Returns
+    -------
+    """
+    print(" testa\n")
+
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
+
+
